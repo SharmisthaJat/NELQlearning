7a2bddb92be124829919505f5129b9161720e43a
sharmisthajat <sharmistha.jat@gmail.com>
2018-03-20 05:07:29 +0530

    random agent added

--- plot.py
+++ plot.py
@@ -2,10 +2,10 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pylab as plt
 
-def plot_reward(rewards):
+def plot_reward(rewards,file_name):
 	fig,ax = plt.subplots()
 	plt.plot(rewards)
 	plt.ylabel('cumulative_reward')
 	plt.xlabel('time')
 	plt.show()
-	fig.savefig("./outputs/reward_plot", bbox_inches='tight')
+	fig.savefig("./outputs/"+file_name, bbox_inches='tight')
