9fb14c7151776cfbfa0ea2ca958dc917605ae113
sharmistha jat <sharmistha.jat@gmail.com>
2018-03-19 12:19:25 -0400

    plotting function added

---
+++ plot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+def plot_reward(rewards):
+	fig,ax = plt.subplots()
+	plt.plot(rewards)
+	plt.ylabel('cumulative_reward')
+	plt.xlabel('time')
+	plt.show()
+	fig.savefig("./outputs/reward_plot", bbox_inches='tight')
